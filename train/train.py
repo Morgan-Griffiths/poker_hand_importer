ce6c740af4a5dbe00d6115eb9c6b9122c210ba8d
Morgan Griffiths <morgan@levelchinese.com>
2023-04-10 14:49:31 -0700

    added weight checkpoints

--- train/train.py
+++ train/train.py
@@ -7,19 +7,26 @@ import torch
 
 
 def train_network(training_params, game_states, target_actions, target_rewards,config):
+    weight_dir = "weights"
     model = Transformer(config.flattened_token_size,config.n_embd,config.n_heads,config.dropout,config.block_size,config.action_size,config.n_layers)
     optimizer = AdamW(model.parameters(), lr=0.0003)
     losses = []
-    for e in range(training_params["epochs"]):
-        out = model(game_states)
-        print("out", out.shape)
-        print('target_actions', target_actions.shape)
-        loss = F.cross_entropy(out, target_actions)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        print(f'Epoch: {e}, loss {loss.item()}')
-        losses.append(loss.item())
+    try:
+        for e in range(training_params["epochs"]):
+            out = model(game_states)
+            print("out", out.shape)
+            print('target_actions', target_actions.shape)
+            loss = F.cross_entropy(out, target_actions)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            print(f'Epoch: {e}, loss {loss.item()}')
+            losses.append(loss.item())
+    except:
+        # save weights
+        torch.save(model.state_dict(), f"{weight_dir}/model_{e}.pth")
+        np.save(f"{weight_dir}/losses_{e}.npy", losses)
+
 
 if __name__ == "__main__":
     config = Config()
